97db2e720641ebd80332199eecee1abd3633c942
Igor Strozzi <istrozzi@matematica.ufrj.br>
2022-01-13 16:34:10 -0300

    ...

--- screener/get_minimal_quantity.py
+++ screener/get_minimal_quantity.py
@@ -26,7 +26,7 @@ tp*leverage
 
 # %%
 qty_formatter = lambda ordersize, qty_precision: f"{float(ordersize):.{qty_precision}f}"
-price_formatter = lambda price, price_precision: f"{float(price):.{price_precision-1}f}"
+price_formatter = lambda price, price_precision: f"{float(price):.{price_precision}f}"
 
 def get_filters():
     with open("symbols_filters.json") as f:
@@ -36,95 +36,17 @@ def process_all_stats(all_stats):
     perps = [pd.DataFrame.from_records([symbol_data]) for symbol_data in all_stats]
     return perps
 
-def apply_symbol_filters(symbol, filters, qty=1.1):
+def apply_symbol_filters(filters, base_price, qty=1.1):
     
-    if symbol in filters.keys():
-
-        price_precision = int(filters["pricePrecision"])
-        qty_precision = int(filters["quantityPrecision"])
-
-        minNotional = 5
-        min_qty = minNotional/base_price
-        order_size = qty * min_qty
-
-        return price_precision, qty_precision, min_qty, order_size
-
-# %%
-
-
-
-
-filters = get_filters()
-
-
-symbolFilters = filters[symbol]
-
-# %%
-
-symbolFilters
-
-# %%
-
-
-stats_24h = client.futures_ticker(symbol=symbol)
-
-base_price = float(stats_24h["lowPrice"])
-
-# %%
-price_precision, qty_precision, min_qty, order_size = apply_symbol_filters(symbol, filters)
-# %%
-# minNotional = 5
-# minQty = float(symbolFilters["minQty"])
-# %%
-# minqty = minNotional/base_price
-
-# %%
-
-# %%
-print(minqty, np.ceil(minqty))
-# %%
-def set_price_filters(symbol, client, filters, qty=1.1):
-    
-
-    
+    price_precision = int(filters["pricePrecision"])    
     qty_precision = int(filters["quantityPrecision"])
-    price_precision = int(filters["pricePrecision"])
-    # print(qty_precision)
-    # print(price_precision)
-    notional = 5
-    min_qty = 1 / 10 ** qty_precision
-    ticker = client.get_symbol_ticker(symbol=symbol)
-    price = float(ticker["price"])
-    multiplier = qty * np.ceil(notional / (price * min_qty))
-    # f"{float(value):.{decimal_count}f}"
-    qty = f"{float(multiplier*min_qty):.{qty_precision}f}"
-    price_formatter = lambda x: f"{float(x):.{price_precision-1}f}"
-    return price_formatter, qty
-    print(min_qty)
-    print(qty)
-    print(price_formatter(price))
 
-# %%
+    minNotional = 5
+    min_qty = minNotional/base_price
+    order_size = qty * min_qty
 
-pf, qty= set_price_filters(symbol, client, symbolFilters)
+    return price_precision, qty_precision, min_qty, order_size
 
-# %%
-qty
-# %%
-new_position = client.futures_create_order(
-        symbol=symbol,
-        side="BUY",
-        type="MARKET",
-        quantity=qty,
-        priceProtect=False,
-        workingType="CONTRACT_PRICE",
-)
-
-#%%
-
-
-# %%
-  
 def compute_exit(entry_price, target_profit, side, entry_fee=0.04, exit_fee=0.04):
     if side == "BUY":
         exit_price = (
@@ -140,8 +62,6 @@ def compute_exit(entry_price, target_profit, side, entry_fee=0.04, exit_fee=0.04
         )
     return exit_price
 
-
-#%%
 def send_order_grid(symbol, tp, side, price_formatter, protect=False, sl=None):
     if side == -1:
         side = "SELL"
@@ -164,16 +84,36 @@ def send_order_grid(symbol, tp, side, price_formatter, protect=False, sl=None):
     )
 
 # %%
+
+filters = get_filters()
+
+
+symbolFilters = filters[symbol]
+print(symbolFilters)
+
+stats_24h = client.futures_ticker(symbol=symbol)
+
+base_price = float(stats_24h["lowPrice"])
+
+# %%
+price_precision, qty_precision, min_qty, order_size = apply_symbol_filters(symbolFilters, base_price, qty=1.1)
+# filtered_stuff = apply_symbol_filters(symbolFilters, base_price, qty=1.1)
+# filtered_stuff
+print(min_qty, np.ceil(min_qty))
+
+# %%
+formatted_order_size = qty_formatter(order_size, qty_precision)
+formatted_order_size
+# %%
+
+
+#%%
+
+
+# %%
 position = client.futures_position_information(symbol=symbol)
 entry_price = float(position[0]["entryPrice"])
-qty = position[0]["positionAmt"]
-#%%
-position
-#%%
-entry_price
-#%%
-qty
-#%%
+position_qty = position[0]["positionAmt"]
 
 # %%
 
@@ -184,14 +124,25 @@ elif side == 1:
     side = "BUY"
     counterside = "SELL"
 
-tp_price = pf(
-    compute_exit(entry_price, tp, side=side)
+tp_price = price_formatter(
+    compute_exit(entry_price, tp, side=side),
+    price_precision,
 )
 print(
     f"""price: {entry_price}
           tp_price: {tp_price}
           """
 )
+
+# %%
+maxQty = 11*min_qty
+
+# %%
+
+formatted_qty = qty_formatter(maxQty, qty_precision)
+# %%
+formatted_qty, tp_price
+# %%
 
 try:
     tp_order = client.futures_create_order(
@@ -200,10 +151,21 @@ try:
         type="LIMIT",
         price=tp_price,
         workingType="CONTRACT_PRICE",
-        quantity=qty,
+        quantity=formatted_qty,
         reduceOnly=True,
         priceProtect=False,
         timeInForce="GTC",
     )
 except BinanceAPIException as error:
     print("tp order, ", error)
+
+
+# %%
+new_position = client.futures_create_order(
+        symbol=symbol,
+        side="BUY",
+        type="MARKET",
+        quantity=formatted_order_size,
+        priceProtect=False,
+        workingType="CONTRACT_PRICE",
+)
